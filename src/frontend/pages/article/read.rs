f523a2ab24b39535d80a6cd3d92037f34a5ce8dd
Felix Ableitner <me@nutomic.com>
2024-01-25 16:26:50 +0100

    render article as markdown

--- src/frontend/pages/article/read.rs
+++ src/frontend/pages/article/read.rs
@@ -2,6 +2,7 @@ use crate::frontend::components::article_nav::ArticleNav;
 use crate::frontend::pages::article_resource;
 use leptos::*;
 use leptos_router::*;
+use markdown_it::MarkdownIt;
 
 #[component]
 pub fn ReadArticle() -> impl IntoView {
@@ -16,14 +17,22 @@ pub fn ReadArticle() -> impl IntoView {
     view! {
         <ArticleNav article=article.clone()/>
         <Suspense fallback=|| view! {  "Loading..." }> {
+            let parser = markdown_parser();
             move || article.get().map(|article|
             view! {
                 <div class="item-view">
                     <h1>{article.article.title.replace('_', " ")}</h1>
-                    <div>{article.article.text}</div>
+                    <div inner_html={parser.parse(&article.article.text).render()}/>
                 </div>
             })
         }
         </Suspense>
     }
+}
+
+fn markdown_parser() -> MarkdownIt {
+    let mut parser = MarkdownIt::new();
+    markdown_it::plugins::cmark::add(&mut parser);
+    markdown_it::plugins::extra::add(&mut parser);
+    parser
 }
