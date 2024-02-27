cbb24ba3d836592696bc294031f690596af05358
Felix Ableitner <me@nutomic.com>
2024-02-27 19:00:42 +0100

    fix killing watch script

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -5,7 +5,7 @@ IBIS__BIND="${IBIS_BIND:-"127.0.0.1:8081"}"
 
 # run processes in parallel
 # https://stackoverflow.com/a/52033580
-(trap 'kill 0' SIGINT;
+(trap 'kill 0' INT;
   # start frontend
   CARGO_TARGET_DIR=target/frontend trunk serve -w src/frontend/ --proxy-backend http://$IBIS__BIND &
   # start backend, with separate target folder to avoid rebuilds from arch change
