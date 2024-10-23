b74cc5ea4f66dcbc8efe9e7cdfc263f7f87e3553
Bintou Ali <bintou005@protonmail.com>
2024-10-23 13:03:37 -0400

    Adds table of contents file

---
+++ src/frontend/pages/article/table_of_contents.rs
@@ -0,0 +1,13 @@
+
+
+
+
+
+struct TableContent {
+    depth: usize,
+    title: String
+}
+
+pub fn generate_table_of_contents(text: &str) -> Vec<TableContent> {
+    let content = text.lines().map()
+}
