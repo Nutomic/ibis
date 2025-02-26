aad60e645bf8ed1f1aecab563b7ed850409f3ea1
Felix Ableitner <me@nutomic.com>
2025-02-26 10:26:11 +0100

    Fix ui rendering

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: { 
-    files: ["*.html", "./src/**/*.rs"],
+    files: ["*.html", "./crates/frontend/**/*.rs"],
   },
   theme: {
       extend: {},
