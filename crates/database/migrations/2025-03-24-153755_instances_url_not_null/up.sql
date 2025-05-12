a1017ce2ec96151f2e07c724c67689536a5d8b46
Felix Ableitner <me@nutomic.com>
2025-05-12 14:20:57 +0200

    Dont drop any db columns during migration

--- crates/database/migrations/2025-03-24-153755_instances_url_not_null/up.sql
+++ crates/database/migrations/2025-03-24-153755_instances_url_not_null/up.sql
@@ -1,2 +1,2 @@
-delete from instance where instances_url is null;
+update instance set instances_url = ap_id where instances_url is null;
 alter table instance alter column instances_url set not null;
