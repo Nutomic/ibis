63ecdc8ef4108bc8671d7691d8215c58c946c35f
Matt Payne <PattMayne@users.noreply.github.com>
2025-10-23 06:32:15 -0400

    Added Debug trait to fix error (#177)
    * added Debug trait
    
    * appeasing the pipeline creature

--- crates/frontend_components/src/utils/mod.rs
+++ crates/frontend_components/src/utils/mod.rs
@@ -2,7 +2,7 @@ use chrono::{Duration, Local};
 use codee::string::FromToStringCodec;
 use leptos::prelude::*;
 use leptos_use::{SameSite, UseCookieOptions, use_cookie_with_options};
-use std::str::FromStr;
+use std::{fmt::Debug, str::FromStr};
 
 pub mod dark_mode;
 pub mod formatting;
@@ -11,7 +11,7 @@ pub mod resources;
 
 pub fn use_cookie<T>(name: &str) -> (Signal<Option<T>>, WriteSignal<Option<T>>)
 where
-    T: Send + Sync + FromStr + ToString + Clone + 'static,
+    T: Debug + Send + Sync + FromStr + ToString + Clone + 'static,
 {
     let expires = (Local::now() + Duration::days(356)).timestamp();
     let cookie_options = UseCookieOptions::default()
