87bbb7087ac3240089d0db27b7357296fff4d098
Felix Ableitner <me@nutomic.com>
2026-04-24 11:12:23 +0200

    Update Dockerfile Debian version (fixes #190)

--- crates/frontend_components/src/article_nav.rs
+++ crates/frontend_components/src/article_nav.rs
@@ -11,20 +11,12 @@ use ibis_api_client::{
 };
 use ibis_database::common::article::{ArticleView, can_edit_article};
 use leptos::prelude::*;
+use leptos_fluent::tr;
 use leptos_meta::Title;
 use leptos_router::components::A;
 use phosphor_leptos::{
-    BELL,
-    BELL_SLASH,
-    BOOK,
-    CHATS_CIRCLE,
-    FEDIVERSE_LOGO,
-    GEAR_SIX,
-    Icon,
-    LIST,
-    LOCK_SIMPLE,
-    PENCIL,
-    TRASH,
+    BELL, BELL_SLASH, BOOK, CHATS_CIRCLE, FEDIVERSE_LOGO, GEAR_SIX, Icon, LIST, LOCK_SIMPLE,
+    PENCIL, TRASH,
 };
 
 #[derive(Clone, Copy, Debug)]
@@ -42,129 +34,129 @@ pub fn ArticleNav(
     active_tab: ActiveTab,
 ) -> impl IntoView {
     view! {
-        <Suspense>
-            {move || Suspend::new(async move {
-                article
-                    .await
-                    .map(|article_| {
-                        let title = article_.article.title();
-                        let article_link = article_path(&article_.article);
-                        let article_link_ = article_link.clone();
-                        let ap_id = article_.article.ap_id.to_string();
-                        let removed = article_.article.removed;
-                        let protected = article_.article.protected;
-                        let pending = article_.article.pending;
-                        let follow_article_action = Action::new(move |_: &()| async move {
-                            CLIENT
-                                .follow_article(article_.article.id, !article_.following)
-                                .await
-                                .error_popup(|_| article.refetch());
-                        });
-                        let follow_title = if article_.following {
-                            "Notifications active"
-                        } else {
-                            "Notifications inactive"
-                        };
-                        view! {
-                            <Title text=page_title(active_tab, &title) />
-                            <div class="tabs tabs-lift md:flex">
-                                <A
-                                    href=article_link.clone()
-                                    exact=true
-                                    {..}
-                                    role="tab"
-                                    class="tab md:flex-auto"
-                                >
-                                    <Icon icon=BOOK />
-                                    "Read"
-                                </A>
-                                <A
-                                    href=format!("{article_link}/discussion")
-                                    {..}
-                                    role="tab"
-                                    class="tab md:flex-auto"
-                                >
-                                    <Icon icon=CHATS_CIRCLE />
-                                    "Discussion"
-                                </A>
-                                <A
-                                    href=format!("{article_link}/history")
-                                    {..}
-                                    role="tab"
-                                    class="tab md:flex-auto"
-                                >
-                                    <Icon icon=LIST />
-                                    "History"
-                                </A>
-                                <Show when=move || {
-                                    is_logged_in()
-                                        && can_edit_article(&article_.article, is_admin()).is_ok()
-                                }>
-                                    <A
-                                        href=format!("{article_link}/edit")
-                                        {..}
-                                        role="tab"
-                                        class="tab md:flex-auto"
-                                    >
-                                        <Icon icon=PENCIL />
-                                        "Edit"
-                                    </A>
-                                </Show>
-                                <Suspense>
-                                    <Show when=is_admin>
-                                        <A
-                                            href=format!("{article_link_}/actions")
-                                            {..}
-                                            role="tab"
-                                            class="tab md:flex-auto"
-                                        >
-                                            <Icon icon=GEAR_SIX />
-                                            "Actions"
-                                        </A>
-                                    </Show>
-                                </Suspense>
-                            </div>
-                            <div class="flex flex-row place-items-center gap-2">
-                                <h1 class="flex-auto my-6 font-serif text-4xl font-bold grow">
-                                    {title}
-                                </h1>
-                                <Pending pending />
-                                <a href=ap_id>
-                                    <Icon icon=FEDIVERSE_LOGO size="24px" />
-                                </a>
-                                <Show when=move || removed>
-                                    <span title="Article was removed">
-                                        <Icon icon=TRASH size="24px" />
-                                    </span>
-                                </Show>
-                                <Show when=move || protected>
-                                    <span title="Article can only be edited by local admins">
-                                        <Icon icon=LOCK_SIMPLE size="24px" />
-                                    </span>
-                                </Show>
-                                <button
-                                    class="btn btn-sm btn-outline"
-                                    on:click=move |_| {
-                                        follow_article_action.dispatch(());
-                                    }
-                                    title=follow_title
-                                >
-                                    <Show
-                                        when=move || article_.following
-                                        fallback=move || {
-                                            view! { <Icon icon=BELL_SLASH size="24px" /> }
+                        <Suspense>
+                            {move || Suspend::new(async move {
+                                article
+                                    .await
+                                    .map(|article_| {
+                                        let title = article_.article.title();
+                                        let article_link = article_path(&article_.article);
+                                        let article_link_ = article_link.clone();
+                                        let ap_id = article_.article.ap_id.to_string();
+                                        let removed = article_.article.removed;
+                                        let protected = article_.article.protected;
+                                        let pending = article_.article.pending;
+                                        let follow_article_action = Action::new(move |_: &()| async move {
+                                            CLIENT
+                                                .follow_article(article_.article.id, !article_.following)
+                                                .await
+                                                .error_popup(|_| article.refetch());
+                                        });
+                                        let follow_title = if article_.following {
+                                            tr!("notification-active")
+                                        } else {
+                                            tr!("notification-inactive")
+                                        };
+                                        view! {
+                                            <Title text=page_title(active_tab, &title) />
+                                            <div class="tabs tabs-lift md:flex">
+                <A
+                    href=article_link.clone()
+                    exact=true
+                    {..}
+                    role="tab"
+                    class="tab md:flex-auto"
+                >
+                    <Icon icon=BOOK />
+                    {tr!("read-tab")}
+                </A>
+            <A
+                href=format!("{article_link}/discussion")
+                {..}
+                role="tab"
+                class="tab md:flex-auto"
+            >
+                <Icon icon=CHATS_CIRCLE />
+                {tr!("discussion-tab")}
+            </A>
+        <A
+            href=format!("{article_link}/history")
+            {..}
+            role="tab"
+            class="tab md:flex-auto"
+        >
+            <Icon icon=LIST />
+            {tr!("history-tab")}
+        </A>
+                                                <Show when=move || {
+                                                    is_logged_in()
+                                                        && can_edit_article(&article_.article, is_admin()).is_ok()
+                                                }>
+    <A
+        href=format!("{article_link}/edit")
+        {..}
+        role="tab"
+        class="tab md:flex-auto"
+    >
+        <Icon icon=PENCIL />
+        {tr!("edit-tab")}
+    </A>
+                                                </Show>
+                                                <Suspense>
+                                                    <Show when=is_admin>
+                                                        <A
+                                                            href=format!("{article_link_}/actions")
+                                                            {..}
+                                                            role="tab"
+                                                            class="tab md:flex-auto"
+                                                        >
+                                                            <Icon icon=GEAR_SIX />
+                                                            "Actions"
+                                                        </A>
+                                                    </Show>
+                                                </Suspense>
+                                            </div>
+                                            <div class="flex flex-row place-items-center gap-2">
+                                                <h1 class="flex-auto my-6 font-serif text-4xl font-bold grow">
+                                                    {title}
+                                                </h1>
+                                                <Pending pending />
+                                                <a href=ap_id>
+                                                    <Icon icon=FEDIVERSE_LOGO size="24px" />
+                                                </a>
+                                                <Show when=move || removed>
+                                                    <span title="Article was removed">
+                                                        <Icon icon=TRASH size="24px" />
+                                                    </span>
+                                                </Show>
+                                                <Show when=move || protected>
+                                                    <span title="Article can only be edited by local admins">
+                                                        <Icon icon=LOCK_SIMPLE size="24px" />
+                                                    </span>
+                                                </Show>
+                                                <button
+                                                    class="btn btn-sm btn-outline"
+                                                    on:click=move |_| {
+                                                        follow_article_action.dispatch(());
+                                                    }
+                                                    title=follow_title
+                                                >
+                                                    <Show
+                                                        when=move || article_.following
+                                                        fallback=move || {
+                                                            view! { <Icon icon=BELL_SLASH size="24px" /> }
+                                                        }
+                                                    >
+                                                        <Icon icon=BELL size="24px" />
+                                                    </Show>
+                                                </button>
+                                            </div>
                                         }
-                                    >
-                                        <Icon icon=BELL size="24px" />
-                                    </Show>
-                                </button>
-                            </div>
-                        }
-                    })
-            })}
+                                    })
+                            })}
 
-        </Suspense>
-    }
+                        </Suspense>
+                    }
 }
 
 fn page_title(active_tab: ActiveTab, article_title: &str) -> String {
--- locales/en/main.ftl
+++ locales/en/main.ftl
@@ -4,3 +4,14 @@ about = About
 create-article = Create Article
 admin-settings = Admin Settings
 notifications = Notifications
+
+# Article navigation strings
+notification-active = Notifications active
+notification-inactive = Notifications inactive
+read-tab = Read
+discussion-tab = Discussion
+history-tab = History
+edit-tab = Edit
+actions-tab = Actions
+article-removed = Article was removed
+article-protected = Article can only be edited by local admins
